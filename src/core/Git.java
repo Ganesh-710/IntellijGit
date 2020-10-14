3cb0c0aef6d681067432b0e74d20d213b51f63a0
ganesh <ganeshravi710@gmail.com>
2020-10-15 05:17:48 +0530

    second

--- src/core/Git.java
+++ src/core/Git.java
@@ -2,7 +2,7 @@ package core;
 
 public class Git {
 
-    public static void main(String[] args) {
-        System.out.println("Git is awesome");
-    }
+  public static void main(String[] args) {
+
+  }
 }
